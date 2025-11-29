f3240871e07dd0b1a797c5e566bd32e34b7c9ffb
venky2809 <jvenkatesh494@gmail.com>
2025-11-29 17:16:01 +0000

    Update S3 bucket name in backend configuration

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "venky28099" # Replace with your actual S3 bucket name
+    bucket = "venkymaluma" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "eu-west-2"
   }
